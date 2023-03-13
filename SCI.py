c25aaea0596d0b18a5348d006c135002c33032e7
Carlos Catarino <carlos_cata27@hotmail.com>
2023-03-13 17:49:33 -0600

    Create dataframe from interface, and same logic
    Create dataframe from data extracted from interface, also save it in a .xlsx file, everything else its same logic to create ID and upload to firebase

--- SCI.py
+++ SCI.py
@@ -15,90 +15,13 @@ from tkcalendar import DateEntry
 #Conexion to Firebase Storage
 import pyrebase
 
-#Interface tkinter
-root = Tk()
+#Define global DataFrame
+DFDatos = pd.DataFrame(columns=['Nombre','Nombre 2','Apellido Paterno','Apellido Materno','Sede','Folio','Registro Postgrado','Vigencia','Numero Empleado'])
 
-root.title("Sistema de credenciales")
-root.geometry("720x720")
-root.iconbitmap("SCI.ico")
-root.config(bg="#900C3F",cursor="arrow") #color y tipo de cursor
+print(len(DFDatos.index))
 
-#DATAFRAME
-Nombre = StringVar()
-nombre = StringVar()
-Nombre2 = StringVar()
-Apellido = StringVar()
-Apellido2 = StringVar()
-Sede = StringVar()
-Folio = StringVar()
-Registro = StringVar()
-Vigencia = StringVar()
-Empleado = StringVar()
-#TITLE
-textTitle = Label(root,text="Sistema de credenciales para la UTEyCV", bd=4,font="arial 16",bg="#900C3F",fg="#fff")
-textTitle.place(x=20,y=50)
-#FORM
-textNombre = Label(root,text="Nombre:", bd=4,font="arial 12",bg="#900C3F",fg="#fff")
-textNombre.place(x=20,y=150)
-inNombre = Entry(root,textvariable=Nombre,bd=4,font="arial 12",bg="#CFCFCF",fg="#000")
-inNombre.place(x=150,y=150)
-textNombre2 = Label(root,text="Nombre 2:", bd=4,font="arial 12",bg="#900C3F",fg="#fff")
-textNombre2.place(x=20,y=200)
-inNombre2 = Entry(root,textvariable=Nombre2,bd=4,font="arial 12",bg="#CFCFCF",fg="#000")
-inNombre2.place(x=150,y=200)
-textApellido = Label(root,text="Apellido:", bd=4,font="arial 12",bg="#900C3F",fg="#fff")
-textApellido.place(x=20,y=250)
-inApellido = Entry(root,textvariable=Apellido,bd=4,font="arial 12",bg="#CFCFCF",fg="#000")
-inApellido.place(x=150,y=250)
-textApellido2 = Label(root,text="Apellido 2:", bd=4,font="arial 12",bg="#900C3F",fg="#fff")
-textApellido2.place(x=20,y=300)
-inApellido2 = Entry(root,textvariable=Apellido2,bd=4,font="arial 12",bg="#CFCFCF",fg="#000")
-inApellido2.place(x=150,y=300)
-textSede = Label(root,text="Sede:", bd=4,font="arial 12",bg="#900C3F",fg="#fff")
-textSede.place(x=20,y=350)
-inSede = Entry(root,textvariable=Sede,bd=4,font="arial 12",bg="#CFCFCF",fg="#000")
-inSede.place(x=150,y=350)
-textFolio = Label(root,text="Folio:", bd=4,font="arial 12",bg="#900C3F",fg="#fff")
-textFolio.place(x=20,y=400)
-inFolio = Entry(root,textvariable=Folio,bd=4,font="arial 12",bg="#CFCFCF",fg="#000")
-inFolio.place(x=150,y=400)
-textRegistro = Label(root,text="Registro:", bd=4,font="arial 12",bg="#900C3F",fg="#fff")
-textRegistro.place(x=20,y=450)
-inRegistro = Entry(root,textvariable=Registro,bd=4,font="arial 12",bg="#CFCFCF",fg="#000")
-inRegistro.place(x=150,y=450)
-textVigencia = Label(root,text="Vigencia:", bd=4,font="arial 12",bg="#900C3F",fg="#fff")
-textVigencia.place(x=20,y=500)
-inVigencia = Entry(root,textvariable=Vigencia,bd=4,font="arial 12",bg="#CFCFCF",fg="#000")
-inVigencia.place(x=150,y=500)
-textEmpleado = Label(root,text="Empleado:", bd=4,font="arial 12",bg="#900C3F",fg="#fff")
-textEmpleado.place(x=20,y=550)
-inEmpleado = Entry(root,textvariable=Empleado,bd=4,font="arial 12",bg="#CFCFCF",fg="#000")
-inEmpleado.place(x=150,y=550)
-#SAVED FUNCTION
-def save():
-    datos = [
-        Nombre.get(),
-        Nombre2.get(),
-        Apellido.get(),
-        Apellido2.get(),
-        Sede.get(),
-        Folio.get(),
-        Registro.get(),
-        Vigencia.get(),
-        Empleado.get()
-    ]
-    print(datos)
-    DFDatosRaw = pd.DataFrame(datos)
-    print(DFDatosRaw)
-    DFDatosRaw2 = DFDatosRaw.transpose()
-    print(DFDatosRaw2)
-
-    saved = Label(root,text="Registro guardado!",bg="yellow")
-    saved.pack()
-    clear()
-    
 #CLEAR INPUTS FUNCTION
-def clear():
+def clear(inNombre,inNombre2,inApellido,inApellido2,inSede,inFolio,inRegistro,inVigencia,inEmpleado):
     inNombre.delete("0","end")
     inNombre2.delete("0","end")
     inApellido.delete("0","end")
@@ -110,13 +33,95 @@ def clear():
     inEmpleado.delete("0","end")
 
 
-buttonName = Button(root,text="Guardar Datos",bd=3,command = save,bg="#94FF40",font="arial 12",cursor="plus")
-buttonName.place(x=20,y=600)
+def Interface():
+        
+    #Interface tkinter
+    root = Tk()
 
-#Cierre de la ventana 
-root.mainloop()
+    root.title("Sistema de credenciales")
+    root.geometry("720x720")
+    root.iconbitmap("SCI.ico")
+    root.config(bg="#900C3F",cursor="arrow") #color y tipo de cursor
 
+    #DATAFRAME
+    Nombre = StringVar()
+    nombre = StringVar()
+    Nombre2 = StringVar()
+    Apellido = StringVar()
+    Apellido2 = StringVar()
+    Sede = StringVar()
+    Folio = StringVar()
+    Registro = StringVar()
+    Vigencia = StringVar()
+    Empleado = StringVar()
+    #TITLE
+    textTitle = Label(root,text="Sistema de credenciales para la UTEyCV", bd=4,font="arial 16",bg="#900C3F",fg="#fff")
+    textTitle.place(x=20,y=50)
+    #FORM
+    textNombre = Label(root,text="Nombre:", bd=4,font="arial 12",bg="#900C3F",fg="#fff")
+    textNombre.place(x=20,y=150)
+    inNombre = Entry(root,textvariable=Nombre,bd=4,font="arial 12",bg="#CFCFCF",fg="#000")
+    inNombre.place(x=150,y=150)
+    textNombre2 = Label(root,text="Nombre 2:", bd=4,font="arial 12",bg="#900C3F",fg="#fff")
+    textNombre2.place(x=20,y=200)
+    inNombre2 = Entry(root,textvariable=Nombre2,bd=4,font="arial 12",bg="#CFCFCF",fg="#000")
+    inNombre2.place(x=150,y=200)
+    textApellido = Label(root,text="Apellido:", bd=4,font="arial 12",bg="#900C3F",fg="#fff")
+    textApellido.place(x=20,y=250)
+    inApellido = Entry(root,textvariable=Apellido,bd=4,font="arial 12",bg="#CFCFCF",fg="#000")
+    inApellido.place(x=150,y=250)
+    textApellido2 = Label(root,text="Apellido 2:", bd=4,font="arial 12",bg="#900C3F",fg="#fff")
+    textApellido2.place(x=20,y=300)
+    inApellido2 = Entry(root,textvariable=Apellido2,bd=4,font="arial 12",bg="#CFCFCF",fg="#000")
+    inApellido2.place(x=150,y=300)
+    textSede = Label(root,text="Sede:", bd=4,font="arial 12",bg="#900C3F",fg="#fff")
+    textSede.place(x=20,y=350)
+    inSede = Entry(root,textvariable=Sede,bd=4,font="arial 12",bg="#CFCFCF",fg="#000")
+    inSede.place(x=150,y=350)
+    textFolio = Label(root,text="Folio:", bd=4,font="arial 12",bg="#900C3F",fg="#fff")
+    textFolio.place(x=20,y=400)
+    inFolio = Entry(root,textvariable=Folio,bd=4,font="arial 12",bg="#CFCFCF",fg="#000")
+    inFolio.place(x=150,y=400)
+    textRegistro = Label(root,text="Registro:", bd=4,font="arial 12",bg="#900C3F",fg="#fff")
+    textRegistro.place(x=20,y=450)
+    inRegistro = Entry(root,textvariable=Registro,bd=4,font="arial 12",bg="#CFCFCF",fg="#000")
+    inRegistro.place(x=150,y=450)
+    textVigencia = Label(root,text="Vigencia:", bd=4,font="arial 12",bg="#900C3F",fg="#fff")
+    textVigencia.place(x=20,y=500)
+    inVigencia = Entry(root,textvariable=Vigencia,bd=4,font="arial 12",bg="#CFCFCF",fg="#000")
+    inVigencia.place(x=150,y=500)
+    textEmpleado = Label(root,text="Empleado:", bd=4,font="arial 12",bg="#900C3F",fg="#fff")
+    textEmpleado.place(x=20,y=550)
+    inEmpleado = Entry(root,textvariable=Empleado,bd=4,font="arial 12",bg="#CFCFCF",fg="#000")
+    inEmpleado.place(x=150,y=550)
+    #SAVED FUNCTION
+    def save():
+        datos = [
+            Nombre.get(),
+            Nombre2.get(),
+            Apellido.get(),
+            Apellido2.get(),
+            Sede.get(),
+            Folio.get(),
+            Registro.get(),
+            Vigencia.get(),
+            Empleado.get()
+        ]
+        #Save data inside DataFrame, each iteration in the interface insert data
+        DFDatos.loc[len(DFDatos.index)]=datos
 
+        saved = Label(root,text="Registro guardado!",bg="yellow")
+        saved.pack()
+        clear(inNombre,inNombre2,inApellido,inApellido2,inSede,inFolio,inRegistro,inVigencia,inEmpleado)
+        
+    buttonName = Button(root,text="Guardar Datos",bd=3,command = save,bg="#94FF40",font="arial 12",cursor="plus")
+    buttonName.place(x=20,y=600)
+
+    #Cierre de la ventana 
+    root.mainloop()
+    
+
+Frame = Interface()
 firebaseConfig = {
     "apiKey": "AIzaSyA_QQZNU0vBCJhhmedvg-X1xDnBi2w44BA",
     "authDomain": "credenciales-uteycv.firebaseapp.com",
@@ -140,55 +145,28 @@ def FechaActualCompleta(date):
     messsage = "{} DE {} DEL {}".format(day, month, year)
     return messsage
 
-def recuperarExcel():
-        nombrearch=fd.askopenfilename(initialdir = "./",title = "Seleccione archivo",filetypes = (("Libro de Excel","*.xlsx"),("Libro de Excel 97 a Excel 2003","*.xls"),("Todos los archivos","*.*")))
-        if nombrearch!='':
-            if(nombrearch.endswith('.xlsx') or nombrearch.endswith('.xls')):
-                sys.exit(1)
+#Begin process to make ID's
 
-# Load excel file to analize, you can select it
-#archivo = pd.ExcelFile('Credenciales.xlsx')
-archivo = fd.askopenfilename(initialdir = "./",title = "Seleccione archivo",filetypes = (("Todos los archivos","*.*"),("Libro de Excel","*.xlsx")))
-print("Opening...")
-if archivo!='':
-    if(not(archivo.endswith('.xlsx') or archivo.endswith('.xls'))):
-        print("opened")
-        sys.exit(1)
-    else:
-        print("No opened")
-
-archivo.getCells().insertRows(2,1)
-
-df = pd.read_excel(archivo,0,usecols='A:I',skiprows=range(1))
-
-#Create DataFrame with our parameters
-DFDatos = pd.DataFrame({
-    'Nombre':df.iloc[:,0],
-    'Nombre 2':df.iloc[:,1],
-    'Apellido Paterno':df.iloc[:,2],
-    'Apellido Materno':df.iloc[:,3],
-    'Sede':df.iloc[:,4],
-    'Folio':df.iloc[:,5],
-    'Registro Postgrado':df.iloc[:,6],
-    'Vigencia':df.iloc[:,7],
-    'Numero Empleado':df.iloc[:,8],
-})
 
 #Data cleaning and normalization
-DFDatos['Nombre'] = DFDatos['Nombre'].replace(pd.NA,0)
-DFDatos = DFDatos[DFDatos['Nombre'] != 0]
+""" DFDatos['Nombre'] = DFDatos['Nombre'].replace(pd.NA,0)
+DFDatos = DFDatos[DFDatos['Nombre'] != 0] """
 
 DFDatos['Nombre 2'] = DFDatos['Nombre 2'].replace(pd.NA,'')
 
-DFDatos['Nombre'] = DFDatos['Nombre'].map(lambda x: x.rstrip(' '))
+""" DFDatos['Nombre'] = DFDatos['Nombre'].map(lambda x: x.rstrip(' '))
 DFDatos['Nombre 2'] = DFDatos['Nombre 2'].map(lambda x: x.lstrip(' '))
 DFDatos['Apellido Paterno'] = DFDatos['Apellido Paterno'].map(lambda x: x.rstrip(' '))
 DFDatos['Apellido Materno'] = DFDatos['Apellido Materno'].map(lambda x: x.lstrip(' '))
-
+ """
 DFDatos['Folio'] = DFDatos['Folio'].map(lambda x: x.lstrip('FOLIO:'))
 
-DFDatos['Numero Empleado'] = DFDatos['Numero Empleado'].astype(int)
+DFDatos['Numero Empleado'] = DFDatos['Numero Empleado'].astype("int64")
 
+DFDatos['Vigencia'] = pd.to_datetime(DFDatos['Vigencia'], format="%d/%m/%Y")
+
+
+DFDatos.to_excel('CredencialesRealizadas.xlsx')
 #Creation and validation directorys to save final files 
 shutil.rmtree('CodigosQR', ignore_errors=True)
 shutil.rmtree('PDFs', ignore_errors=True)
